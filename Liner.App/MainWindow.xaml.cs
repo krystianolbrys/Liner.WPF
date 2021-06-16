a7122dda19696337137d3f6ca514349088a400d4
Krystian Olbrys <x11.crs.539@gmail.com>
2021-06-16 12:45:32 +0200

    Final 0.2

--- Liner.App/MainWindow.xaml.cs
+++ Liner.App/MainWindow.xaml.cs
@@ -18,9 +18,10 @@ using Contracts = Liner.API.Contracts;
 /// For that project some fetures should be implemented but i didn't have so much time:
 /// 0.1 - Liner.API for simplicity are class libraries, in production env should be external API and publicated contracts
 /// 0.2 - Proper UnitTest should be written for whole Lines.Core domain business logic models
+/// 0.3 - UnitTests with testing framework like NUnit, libraries - AuotFixture, FluentAssertions, Moq
 /// 1 - ILogger implementation for storing logs in some persist storage
 /// 2 - Some kind of AoP approach for logging business Exceptions from API
-/// 3 - Automapper for mapping Contracts <-> CommandRequests <-> BuildCoreDomainModels (I preffer explicit creation of DomainModels)
+/// 3 - Maybe AutoMapper library for mapping Contracts <-> CommandRequests <-> BuildCoreDomainModels (I preffer explicit creation of DomainModels)
 /// 4.1 - BFS would be significantly faster with dedicated nodes not Node<TValue>, explicit memory allocations(unsafe)
 /// 4.2 - In this case data volume is very small - only 206k nodes with max 4 childrens
 /// 4.2 - For bigger volume more complex algorithm should be used including paraller execution
